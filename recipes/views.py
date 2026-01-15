3d1f7e3b9325be899ffc80a108c66de47e714e1d
Lucas <lucasalvesgnecco@gmail.com>
2026-01-15 19:08:59 -0300

    url do card

--- recipes/views.py
+++ recipes/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'recipes/pages/home.html', context={'name': 'Teste'})
+
+def recipe(request, id):
+    return render(request,'recipes/pages/recipe-view.html', context={'name': 'Teste'})
