47ac80c4e0258bae0653f83676c483418b299409
Lucas <lucasalvesgnecco@gmail.com>
2026-01-17 15:50:00 -0300

    Page not found para category

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import render, get_list_or_404
 from utils.recipes.factory import make_recipe
-from .models import Recipe
+
+from recipes.models import Recipe
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
@@ -10,10 +12,14 @@ def home(request):
         })
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(is_published=True, category__id=category_id).order_by('-id')
+    recipes = get_list_or_404(Recipe.objects.filter(is_published=True, category__id=category_id).order_by('-id'))
+
+    # if not recipes:
+    #     raise Http404('Not found 😭')
 
     return render(request,'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category | ', # type: ignore
         })
 
 
