74ccc43fe6aeb1fa63c3a1a87887f80500d6cd0c
Lucas <lucasalvesgnecco@gmail.com>
2026-01-21 21:18:03 -0300

    teste entrada da base de dados

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,4 +1,6 @@
 from .test_recipe_base import RecipeTestBase
+from parameterized import parameterized
+
 from django.core.exceptions import ValidationError
 
 class RecipeModelTeste(RecipeTestBase):
@@ -9,5 +11,16 @@ class RecipeModelTeste(RecipeTestBase):
     def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
         self.recipe.title = 'A' * 70
 
+        with self.assertRaises(ValidationError):
+            self.recipe.full_clean()
+    
+    @parameterized.expand([
+            ('title', 65),
+            ('description', 165),
+            ('preparation_time_unit', 65),
+            ('service_unit', 65),
+        ])
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
