b4de55f2fe3162b5aee44f88bbf24b9b61a86248
Lucas <lucasalvesgnecco@gmail.com>
2026-01-27 19:53:38 -0300

    adicionando variaveis de sistema (dotenv)

--- recipes/views.py
+++ recipes/views.py
@@ -8,7 +8,7 @@ from recipes.models import Recipe
 
 import os
 
-PER_PAGE = os.environ.get('PER_PAGE', 6)
+PER_PAGE = int(os.environ.get('PER_PAGE', 6))
  
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
