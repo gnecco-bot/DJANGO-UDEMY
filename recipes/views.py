d720e593d503b211078ebb08a58dd3044e078e3a
Lucas <lucasalvesgnecco@gmail.com>
2026-01-27 20:45:42 -0300

    corrigido max-width paga telas menores

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,9 @@
 import os
 
-from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from utils.pagination import make_pagination
-from django.contrib import messages
 
 from recipes.models import Recipe
 
@@ -15,8 +13,6 @@ def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     
     page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
-
-    messages.success(request, 'Epa, você foi pesquisar algo que eu vi.')
 
     return render(request,'recipes/pages/home.html', context={
         'recipes': page_obj,
