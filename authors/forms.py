72059cd9089f8e7219b45db9c3b051ae6350d027
Lucas <lucasalvesgnecco@gmail.com>
2026-02-04 20:27:15 -0300

    email unico no cadastro

--- authors/forms.py
+++ authors/forms.py
@@ -96,6 +96,15 @@ class RegisterForm(forms.ModelForm):
             'password',
         ]
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email', '')
+        exists = User.objects.filter(email=email).exists()
+
+        if exists:
+            raise ValidationError('User e-mail is already in use', code='invalid',)
+
+        return email
+
     def clean(self): # type: ignore
         cleaned_data = super().clean()
 
