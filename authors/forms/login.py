ab3c4511f813627a16b55525c1b77e8cc823757a
Lucas <lucasalvesgnecco@gmail.com>
2026-02-07 11:57:06 -0300

    authenticandousername

--- authors/forms/login.py
+++ authors/forms/login.py
@@ -1,6 +1,7 @@
 from django import forms 
 from utils.django_forms import add_placeholder
 
+
 class LoginForm(forms.Form):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
