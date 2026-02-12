ed1ddd5e430c11a355c41b3afef1bd486458cba0
Lucas <lucasalvesgnecco@gmail.com>
2026-02-12 20:18:42 -0300

    slug das recipes baseada no title

--- recipes/admin.py
+++ recipes/admin.py
@@ -12,6 +12,9 @@ class RecipeAdmin(admin.ModelAdmin):
     list_filter = 'category', 'author', 'is_published', 'preparation_steps_is_html'
     list_per_page = 10
     list_editable = 'is_published',
-    ordering = 'id',
+    ordering = '-id',
+    prepopulated_fields = {
+        "slug": ('title',)
+    }
 
 admin.site.register(Category, CategoryAdmin)
