c8e91e4383b0163e5a0d80b47faa8259786f2f5c
Lucas <lucasalvesgnecco@gmail.com>
2026-02-12 20:15:57 -0300

    listagem de recipes area adm

--- recipes/admin.py
+++ recipes/admin.py
@@ -6,6 +6,12 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['id', 'title', 'created_at', 'is_published', 'author']
+    list_display_links = 'author', 'title'
+    search_fields = 'id', 'title', 'description', 'slug', 'preparation_steps'
+    list_filter = 'category', 'author', 'is_published', 'preparation_steps_is_html'
+    list_per_page = 10
+    list_editable = 'is_published',
+    ordering = 'id',
 
 admin.site.register(Category, CategoryAdmin)
