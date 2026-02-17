c8e67d3b46572f5a1f781aba6521eddb36692ef2
Lucas <lucasalvesgnecco@gmail.com>
2026-02-17 11:00:16 -0300

    resgatando recipes em json

--- recipes/views.py
+++ recipes/views.py
@@ -4,6 +4,7 @@ from django.db.models.query import QuerySet
 from django.http import Http404
 from utils.pagination import make_pagination
 from django.views.generic import ListView, DetailView
+from django.http import JsonResponse
 from recipes.models import Recipe
 
 
@@ -37,6 +38,18 @@ class RecipeListViewBase(ListView):
 
 class RecipeListViewHome(RecipeListViewBase):
     template_name = 'recipes/pages/home.html'
+    
+class RecipeListViewHomeApi(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+    def render_to_response(self, context, **response_kwargs):
+        recipes = self.get_context_data()['recipes']
+        recipes_list = recipes.object_list.values()
+
+        return JsonResponse(
+            list(recipes_list),
+            safe=False,
+            )
 
 class RecipeListViewCategory(RecipeListViewBase):
     template_name = 'recipes/pages/category.html'
