29420861223f361dd25c6b40b938a1298149db27
Lucas <lucasalvesgnecco@gmail.com>
2026-02-17 11:17:34 -0300

    RecipeDetailApi json para visualizar dados de uma recipe

--- recipes/views.py
+++ recipes/views.py
@@ -2,11 +2,12 @@ import os
 from django.db.models import Q
 from django.db.models.query import QuerySet
 from django.http import Http404
+from django.http.response import HttpResponse as HttpResponse
 from utils.pagination import make_pagination
 from django.views.generic import ListView, DetailView
 from django.http import JsonResponse
 from recipes.models import Recipe
-
+from django.forms.models import model_to_dict
 
 PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
@@ -123,6 +124,28 @@ class RecipeDetail(DetailView):
         })
                     
         return ctx
+        
+class RecipeDetailApi(RecipeDetail):
+    def render_to_response(self, context, **response_kwargs):
+        recipe = self.get_context_data()['recipe']
+        recipe_dict = model_to_dict(recipe)
+
+        # Convertendo para string a chave "created_at" e "updated_at"
+        recipe_dict['created_at'] = str(recipe.created_at)
+        recipe_dict['updated_at'] = str(recipe.updated_at)
+
+        if recipe_dict.get('cover'):
+            recipe_dict['cover'] = self.request.build_absolute_uri() + recipe_dict['cover'].url[1:]
+        else:
+            recipe_dict['cover'] = ''
+
+        # Deletando is_published do arquivo json
+        del recipe_dict['is_published']
+
+        return JsonResponse(
+            recipe_dict,
+            safe=False,
+        )
         
     
 # def recipe(request, id):
