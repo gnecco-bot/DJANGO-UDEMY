c6277d4a3f14bef72d3d4f35fdbe3e01b0d98f38
Lucas <lucasalvesgnecco@gmail.com>
2026-02-22 10:54:55 -0300

    vinculo de author.profile na recipe com link direto

--- recipes/views.py
+++ recipes/views.py
@@ -73,8 +73,8 @@ class RecipeListViewBase(ListView):
             is_published=True,
         )
         # Seleciona apenas uma vez o dado da BD e reutiliza
-        qs = qs.select_related('author', 'category')
-        qs = qs.prefetch_related('tags')
+        qs = qs.select_related('author', 'category', 'author__profile')
+        qs = qs.prefetch_related('tags', 'author__profile')
         return qs
     
     def get_context_data(self, *args, **kwargs):
