8fb225fca4031e2354b812630b12657fe637521b
Lucas <lucasalvesgnecco@gmail.com>
2026-02-23 19:54:09 -0300

    signal de recipes para deletar imagem quando for mandada outra

--- recipes/signals.py
+++ recipes/signals.py
@@ -1,7 +1,8 @@
-from django.db.models.signals import pre_delete
+import os
+
+from django.db.models.signals import pre_delete, pre_save
 from django.dispatch import receiver
 from recipes.models import Recipe
-import os
 
 def delete_cover(instance):
     try:
@@ -13,3 +14,12 @@ def delete_cover(instance):
 def recipe_cover_delete(sender, instance, *args, **kwargs):
     old_instance = Recipe.objects.get(pk=instance.pk)
     delete_cover(old_instance)
+
+@receiver(pre_save, sender=Recipe)
+def recipe_cover_update(sender, instance, *args, **kwargs):
+    old_instance = Recipe.objects.get(pk=instance.pk)
+    is_new_cover = old_instance.cover != instance.cover
+
+    if is_new_cover:
+        delete_cover(old_instance)
+    print('Troque a imagem?', is_new_cover)
