976a98df309a92ebf99f3b4d9e329dfd4b80d127
Lucas <lucasalvesgnecco@gmail.com>
2026-03-05 20:13:35 -0300

    test unitario DRF API paginacao esperada

--- recipes/tests/teste_recipe_api.py
+++ recipes/tests/teste_recipe_api.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from rest_framework import test
 from recipes.tests.test_recipe_base import RecipeMixin
+from unittest.mock import patch
 
 class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
     def test_recipe_api_list_returns_status_code_200(self):
@@ -10,3 +11,20 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
             response.status_code,
             200
         )
+    
+    @patch('recipes.views.api.RecipeAPIv2Pagination.page_size', new=7)
+    def test_recipe_api_list_loads_correct_number_of_recipes(self):
+        # ajuste
+        wanted_number_of_recipes = 7
+        self.make_recipe_in_batch(qtd=wanted_number_of_recipes)
+
+        # ação
+        response = self.client.get(reverse('recipes:recipes-api-list') + '?page=1')
+        qtd_of_loaded_recipes = len(response.data.get('results')) # type: ignore
+
+        # asserção
+        self.assertEqual(
+            wanted_number_of_recipes,
+            qtd_of_loaded_recipes
+        )
+    
