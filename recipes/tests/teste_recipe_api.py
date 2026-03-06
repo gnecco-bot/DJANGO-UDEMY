23ec046be0873c2a36afd57001f3b780cc80fc5e
Lucas <lucasalvesgnecco@gmail.com>
2026-03-06 19:50:11 -0300

    test unitario DRF verificando se user esta com token para visualizer a response da api

--- recipes/tests/teste_recipe_api.py
+++ recipes/tests/teste_recipe_api.py
@@ -4,8 +4,12 @@ from recipes.tests.test_recipe_base import RecipeMixin
 from unittest.mock import patch
 
 class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
-    def get_recipe_api_list(self, reverse_result=None):
+    def get_recipe_reverse_url(self, reverse_result=None):
         api_url = reverse_result or reverse('recipes:recipes-api-list')
+        return api_url
+
+    def get_recipe_api_list(self, reverse_result=None):
+        api_url = self.get_recipe_reverse_url(reverse_result)
         response = self.client.get(api_url)
         return response
 
@@ -45,20 +49,36 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
     
     @patch('recipes.views.api.RecipeAPIv2Pagination.page_size', new=10)
     def test_recipe_api_list_loads_recipes_by_category_id(self):
+        # Creates categories
         category_wanted = self.make_category(name='WANTED_CATEGORY')
         category_not_wanted = self.make_category(name='NOT_WANTED_CATEGORY')
+        # Creates 10 recipes
         recipes = self.make_recipe_in_batch(qtd=10)
 
+        # Change all recipes to the wanted category
         for recipe in recipes:
             recipe.category = category_wanted
             recipe.save()
+            
+            # Change one recipe to the NOT wanted category
+            # As a result, this recipe should NOT show in the page 
             recipes[0].category = category_not_wanted
             recipes[0].save()
 
-        api_url = reverse('recipes:recipes-api-list') + f'?category_id={category_wanted.id}'
+        # Action: get recipes by wanted category_id
+        api_url = reverse('recipes:recipes-api-list') + f'?category_id={category_wanted.id}' # type: ignore
         response = self.get_recipe_api_list(reverse_result=api_url)
 
+        # We should only see recipes froom the wanted category
         self.assertEqual(
-            len(response.data.get('results')),
+            len(response.data.get('results')), # type: ignore
             9
+        )
+
+    def test_recipe_api_list_user_must_send_jwt_token_to_create_recipe(self):
+        api_url = self.get_recipe_reverse_url()
+        response = self.client.post(api_url)
+        self.assertEqual(
+            response.status_code,
+            401
         )
