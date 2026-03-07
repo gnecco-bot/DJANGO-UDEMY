ebde3f2a1ce20d33484b4d46a4199efd641fd267
Lucas <lucasalvesgnecco@gmail.com>
2026-03-07 10:37:06 -0300

    CORS HEADERS permitindo sites terceiros acessar API

--- project/settings/installed_apps.py
+++ project/settings/installed_apps.py
@@ -5,6 +5,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # CORS headers
+    "corsheaders",
     # Django rest framework
     'rest_framework',
     'rest_framework_simplejwt',
